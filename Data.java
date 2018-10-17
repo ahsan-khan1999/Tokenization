43f7c18d68d8b426d3590187215492b97403e3f9
Ahsan Khan <36322910+ahsan-khan1999@users.noreply.github.com>
2018-10-17 02:50:42 -0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,18 @@
+public class Data {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+//    public String[] splits(){
+//        String myString = "My Name Is Ahsan";
+//        String delimtr = " ";
+//
+//        String[] myArray = myString.split(delimtr);
+//
+//        return myArray;
+//    }
+}
